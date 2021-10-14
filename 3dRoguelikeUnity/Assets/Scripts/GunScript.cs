330a1b858bf5c59b5f9d01d3ca8477b023a86ff5
LK <49055889+Lanpak@users.noreply.github.com>
2021-10-15 10:02:26 +1100

    Gun Logic Improved, Animations Improved

--- 3dRoguelikeUnity/Assets/Scripts/GunScript.cs
+++ 3dRoguelikeUnity/Assets/Scripts/GunScript.cs
@@ -4,11 +4,29 @@ using UnityEngine.UI;
 public class GunScript : MonoBehaviour
 {
     [Header("Gun Settings")]
+    [SerializeField] private string name;
     [SerializeField] private float damage;
     [SerializeField] private float firingSpeed;
     [SerializeField] private float reloadSpeed;
     [SerializeField] private bool fullAuto;
     [SerializeField] private int magSize;
+
+    [Header("Shotgun Settings")]
+    [SerializeField] private bool isShotgun = false;
+    [SerializeField] private int bulletsPerShot = 6;
+    [SerializeField] private float spreadX;
+    [SerializeField] private float spreadY;
+    [SerializeField] private float spreadZ;
+
+
+
+
+
+    [Header("Sway")]
+    public float amount;
+    public float maxAmount;
+    public float smoothAmount;
+    
 
 
     [Header("Recoil")]
@@ -26,13 +44,31 @@ public class GunScript : MonoBehaviour
     [SerializeField] private Recoil recoilScript;
     [SerializeField] private Animator anim;
 
+
+    [HideInInspector] public int bulletsInMag;
+    
+
     private float nextTimeToFire = 0;
+    private Vector3 initialPosition;
+    private AnimatorStateInfo info;
+    private bool isReloading = false;
 
-
+    void Start()
+    {
+        initialPosition = transform.localPosition;
+        bulletsInMag = magSize;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        HandleSway();
+
+        if(bulletsInMag != magSize && Input.GetButtonDown("Reload"))
+        {
+            Reload();
+        } 
+
         if (fullAuto)
         {
             if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
@@ -49,29 +85,97 @@ public class GunScript : MonoBehaviour
                 Fire();
             }
         }
-        if(Time.time <= nextTimeToFire)
-        {
-            anim.SetBool("isShooting", true);
-        }
-        else
-        {
-
-            anim.SetBool("isShooting", false);
-
-        }
-        
-    
     }
+
+
+    void FixedUpdate()
+    {
+        info = anim.GetCurrentAnimatorStateInfo(0);
+
+        isReloading = info.IsName(name + "_reload");
+    }
+
 
 
     private void Fire()
     {
-        recoilScript.RecoilFire();
-        
-        RaycastHit hit;
-        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit))
+        if (isReloading)
         {
-            Debug.Log(hit.transform.name);
+            return;
         }
+        if (bulletsInMag == 0)
+        {
+            Reload();
+        }
+        else
+        {
+            if (isShotgun)
+            {
+                bulletsInMag--;
+                recoilScript.RecoilFire();
+                anim.Play(name + "_fire");
+                for (int i = 0; i < bulletsPerShot; i++)
+                {
+                    
+                    RaycastHit hit;
+                    if (Physics.Raycast(camera.transform.position,HandleSpread(), out hit))
+                    {
+                        Debug.Log(hit.transform.name);
+                    }
+                }
+            }
+            else
+            {
+                bulletsInMag--;
+                recoilScript.RecoilFire();
+                anim.Play(name + "_fire");
+                RaycastHit hit;
+                if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit))
+                {
+                    Debug.Log(hit.transform.name);
+                }
+            }
+
+
+            
+        }  
+    }
+
+    private void Reload()
+    {
+        if(isReloading)
+        {
+            return;
+        }
+        anim.Play(name + "_reload");
+        bulletsInMag = magSize;
+    }
+
+    private void HandleSway()
+    {
+        float movementX = Input.GetAxis("Mouse X") * amount;
+        float movementY = Input.GetAxis("Mouse Y") * amount;
+
+        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
+        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
+
+
+        Vector3 finalPosition = new Vector3(movementX, movementY, 0);
+
+        transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * smoothAmount);
+    }
+
+
+    private Vector3 HandleSpread()
+    {
+        Vector3 targetPos = camera.transform.position + camera.transform.forward;
+        targetPos = new Vector3
+            (
+            targetPos.x + Random.Range(-spreadX, spreadX),
+            targetPos.y + Random.Range(-spreadY, spreadY),
+            targetPos.z + Random.Range(-spreadZ, spreadZ)
+            );
+        Vector3 direction = targetPos - camera.transform.position;
+        return direction.normalized;
     }
 }
